93e7b9c9766eaa760f4bac8ead963430f186ccb6
palo1992 <65390185+palo1992@users.noreply.github.com>
2020-12-08 22:40:56 +0100

    finished sign in and sign up, fix variable item in collection-preview also requried atr

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -7,7 +7,7 @@ const CollectionPreview = ({title, items}) => (
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className="preview">
             {
-                items.filter((idem, idx) => idx < 4)
+                items.filter((item, idx) => idx < 4)
                 .map(({id, ...otherItemProps}) => (
                     <CollectionItem key={id} {...otherItemProps}/>
                 ))
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -15,14 +15,24 @@ class SignIn extends React.Component {
             password: ''
         }
     }
-    handleSubmit = event => {
+
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({ email: '', password: '' })
+
+        const {email, password} = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: '', password: '' })
+        } catch (error) {
+           console.error(error); 
+        }
     }
+
     handleChange = event => {
         const { value, name } = event.target;
         this.setState({ [name]: value })
     }
+
     render() {
         return (
             <div className="sign-in">
@@ -35,7 +45,7 @@ class SignIn extends React.Component {
                         value={this.state.email}
                         handleChange={this.handleChange}
                         label="email"
-                        required="true"
+                        required
                     />
                     <FormInput
                         name="password"
@@ -43,7 +53,7 @@ class SignIn extends React.Component {
                         value={this.state.password}
                         handleChange={this.handleChange}
                         label="password"
-                        required="true"
+                        required
                     />
                     <div className="buttons">
                         <CustomButton type="submit" >sign in</CustomButton>
