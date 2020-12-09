a4bc155213ba68038a79b1ee155d3cda607d56ab
palo1992 <65390185+palo1992@users.noreply.github.com>
2020-12-09 22:51:23 +0100

    added directory selector and reducer

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,59 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+import {selectDirectorySections} from '../../redux/directory/directory.selectors';
+
 import MenuItem from '../menu-item/menu-item.component';
 import './directory.styles.scss';
 
-class Directory extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            sections: [
-                {
-                    title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id: 1,
-                    linkUrl: 'hats'
-                },
-                {
-                    title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                    id: 2,
-                    linkUrl: 'jackets'
-                },
-                {
-                    title: 'sneakers',
-                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                    id: 3,                    
-                    linkUrl: 'sneakers'
-                },
-                {
-                    title: 'womens',
-                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                    size: 'large',
-                    id: 4,                    
-                    linkUrl: 'womens'
-                },
-                {
-                    title: 'mens',
-                    imageUrl: 'https://i.ibb.co/R70vBrQ/mens.png',
-                    size: 'large',
-                    id: 5,                    
-                    linkUrl: 'mens'
-                }
-            ]
-        }
-    }
-    render(){
-        return (
-            <div className="directory-menu">
-               {/* { this.state.sections.map(({title,imageUrl,id,size,linkUrl}) => (
+const Directory = ({sections}) => (
+    <div className="directory-menu">
+        {/* { this.state.sections.map(({title,imageUrl,id,size,linkUrl}) => (
                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl}/>
                ))} */
-               this.state.sections.map(({id,...otherSectionProps}) => (
-                <MenuItem key={id} {...otherSectionProps}/>
+            sections.map(({ id, ...otherSectionProps }) => (
+                <MenuItem key={id} {...otherSectionProps} />
             ))}
-            </div>
-        );
-    }
-}
+    </div>
+);
 
-export default Directory;
+const mapStateToProps = createStructuredSelector({
+    sections: selectDirectorySections
+});
+
+export default connect(mapStateToProps)(Directory);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-import {Provider} from 'react-redux';
-import store from './redux/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import {store, persistor} from './redux/store';
 
 import App from './App';
 
@@ -12,7 +13,9 @@ import './index.css';
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
+    <PersistGate persistor={persistor}>
       <App />
+    </PersistGate>
     </BrowserRouter>
   </Provider>
   ,document.getElementById('root')
