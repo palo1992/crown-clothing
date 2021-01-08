b0d0bfaab43902b72e5fc78a83b64b4dcc7043b9
palo1992 <65390185+palo1992@users.noreply.github.com>
2021-01-08 18:30:20 +0100

    remove git ignore files

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({price}) => {
     const priceForStripe = price * 100;
     const publishableKey= 'pk_test_51HwnywLsKwomMPMZow5nliFjQ98h5iHJmKdtymi8jfZaPpnLfbGAKiMQ8JcZ7s1b3A8leP7gPRMRZO2nCk2nCVvi00TkukNdPI';
+    
     const onToken = token =>{
-    console.log(token);
-    alert('Payment Successful');
-}
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment was successful');
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error));
+            alert('There was an issue with your payment. Please make sure you use the provided credit card.');
+        });
+    };
     return (
         <StripeCheckout 
             label='Pay Now'
