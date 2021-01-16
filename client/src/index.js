d0096dae412af350a7bebca7916d1b3f21c303d9
palo1992 <65390185+palo1992@users.noreply.github.com>
2021-01-16 19:50:31 +0100

    added service worker, update manifest, http to https convert

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
+import * as serviceWorker from './service-worker';
 
 import App from './App';
 
@@ -20,3 +21,5 @@ ReactDOM.render(
   </Provider>
   , document.getElementById('root')
 );
+
+serviceWorker.register();
